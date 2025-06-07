ba016b977a625fe0d4e32b5787637059c4c54384
Sahil Mathew <sahilmathew100@gmail.com>
2025-06-07 13:16:36 +0530

    Epic1Story1 added generateUniqueStockSymbols

--- StockMarketTickerSimulator/Program.cs
+++ StockMarketTickerSimulator/Program.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using DataGenerationEngine;
 namespace StockMarketTickerSimulator
 {
 	internal class Program
 	{
 		static void Main( string[] args )
 		{
+			UniqueStockSymbols uniqueStockSymbols = new UniqueStockSymbols();
+			uniqueStockSymbols.generateUniqueStockSymbols();
 		}
 	}
 }
